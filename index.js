69f44cf443c1de1ef89004166569368a085028ac
adam.lusted <adam.lusted@isobar.com>
2018-01-31 12:43:06 +1100

    yup

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
-var fetch = require('node-fetch');
-
+const fetch = require('node-fetch');
 const _ = require('lodash');
 const chalk = require('chalk');
-const log = console.log;
+const leftpad = require('leftpad');
+const rightpad = require('rightpad');
 
-var url = 'https://api.coinmarketcap.com/v1/ticker/';
+class Crypto {
+    constructor() {
+        this.url = 'https://api.coinmarketcap.com/v1/ticker/';
+        this.data = {};
 
-const dataTable = {
-    btc: { name: 'bitcoin', shorthand: 'btc' },
-};
+        this.fetchData(() => {
+            this.results();
+        });
+    }
 
-var data = {};
+    results() {
+        //           BTC: $9850.18     -9.74%  -11.93%   -1.94%
+        console.log(`Sym  Price        7D      24HR      1HR`);
+        console.log(`------------------------------------------`);
+        for (let rank = 1; rank < 11; rank++) {
+            this.logData(_.head(_.filter(this.data, { rank: String(rank) })));
+        }
+    }
 
-console.log('running');
+    logData(dataRow) {
+        const change7 = dataRow.percent_change_7d.indexOf('-') >= 0 ? 'red' : 'green';
+        const change24 = dataRow.percent_change_24h.indexOf('-') >= 0 ? 'red' : 'green';
+        const change1 = dataRow.percent_change_1h.indexOf('-') >= 0 ? 'red' : 'green';
+        const price = rightpad('$'+dataRow.price_usd, 10, ' ');
 
+        console.log(`${dataRow.symbol}: ${chalk.yellow(price)} ${chalk[change7](leftpad(dataRow.percent_change_7d+'%',8,' '))} ${chalk[change24](leftpad(dataRow.percent_change_24h+'%', 8, ' '))} ${chalk[change1](leftpad(dataRow.percent_change_1h+'%',8,' '))}`);
+    }
 
-fetch(url)
-    .then(function(res) {
-        return res.json();
-    }).then(function (json) {
-        data = json;
+    fetchData(cb) {
+        const self = this;
+        fetch(self.url).then(function(res) {
+            return res.json();
+        }).then(function (json) {
+            self.data = json;
+            cb();
+        });
+    }
+}
 
-        var eth = _.head(_.filter(data, { symbol: 'ETH' }));
-
-        console.log(eth);
-
-        var change7 = eth.percent_change_7d.indexOf('-') >= 0 ? 'red' : 'green';
-        var change24 = eth.percent_change_24h.indexOf('-') >= 0 ? 'red' : 'green';
-        var change1 = eth.percent_change_1h.indexOf('-') >= 0 ? 'red' : 'green';
-
-        log(`ETH: ${chalk.yellow('$'+eth.price_usd)}, 7D: ${chalk[change7](eth.percent_change_7d+'%')} 24HR: ${chalk[change24](eth.percent_change_24h+'%')} 1HR: ${chalk[change1](eth.percent_change_1h+'%')}
-        `);
-    });
+new Crypto();
