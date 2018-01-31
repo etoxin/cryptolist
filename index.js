788bb8b063562d5213882031fb4da7bbc933eeb8
adam.lusted <adam.lusted@isobar.com>
2018-01-31 14:53:17 +1100

    Added conversion.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const chalk = require('chalk');
+const program = require('commander');
 const leftpad = require('leftpad');
 const rightpad = require('rightpad');
 
@@ -11,7 +12,13 @@ const rightpad = require('rightpad');
  */
 class Crypto {
     constructor() {
-        this.url = 'https://api.coinmarketcap.com/v1/ticker/';
+        program.option('-c, --convert [con]', 'Convert currency')
+               .parse(process.argv);
+
+        console.log(program.convert);
+
+        this.convert = program.convert ? program.convert : 'usd';
+        this.url = `https://api.coinmarketcap.com/v1/ticker/?convert=${this.convert}`;
         this.data = {};
 
         this.fetchData(() => {
@@ -24,7 +31,7 @@ class Crypto {
      */
     results() {
         //           BTC: $9850.18     -9.74%  -11.93%   -1.94%
-        console.log(`Sym  Price (USD)   7D      24HR      1HR`);
+        console.log(`Sym  Price (${this.convert})   7D      24HR      1HR`);
         console.log(`------------------------------------------`);
         for (let rank = 1; rank < 11; rank++) {
             this.logData(_.head(_.filter(this.data, { rank: String(rank) })));
@@ -38,9 +45,9 @@ class Crypto {
         const change7 = dataRow.percent_change_7d.indexOf('-') >= 0 ? 'red' : 'green';
         const change24 = dataRow.percent_change_24h.indexOf('-') >= 0 ? 'red' : 'green';
         const change1 = dataRow.percent_change_1h.indexOf('-') >= 0 ? 'red' : 'green';
-        const price = rightpad('$'+dataRow.price_usd, 10, ' ');
+        const price = rightpad(dataRow['price_'+this.convert], 10, ' ');
 
-        console.log(`${dataRow.symbol}: ${chalk.yellow(price)} ${chalk[change7](leftpad(dataRow.percent_change_7d+'%',8,' '))} ${chalk[change24](leftpad(dataRow.percent_change_24h+'%', 8, ' '))} ${chalk[change1](leftpad(dataRow.percent_change_1h+'%',8,' '))}`);
+        console.log(`${dataRow.symbol}: ${chalk.yellow(price)} ${chalk[change7](leftpad(dataRow.percent_change_7d+'%',20,' '))} ${chalk[change24](leftpad(dataRow.percent_change_24h+'%', 8, ' '))} ${chalk[change1](leftpad(dataRow.percent_change_1h+'%',8,' '))}`);
     }
 
     /**
